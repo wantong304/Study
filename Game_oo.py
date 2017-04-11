697d0996deea874cc9cd82747bf54678c89c58a4
wantong304 <wangtong304@163.com>
2017-04-11 21:45:05 +0800

    version 1
    The first code

---
+++ Game_oo.py
@@ -0,0 +1,6 @@
+import random
+
+class Sprite;
+
+    s
+
---
+++ Test.py
@@ -0,0 +1,7 @@
+#A first python script
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'spam!'
+print(x * 8)
+input('press enter to exit')
